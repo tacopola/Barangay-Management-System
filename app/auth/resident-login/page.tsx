000d90285fd415b799ecf8a226af1ae422fbd376
tacopola <johnmarkalvis02@gmail.com>
2026-05-12 21:37:54 +0800

    test

--- app/auth/resident-login/page.tsx
+++ app/auth/resident-login/page.tsx
@@ -27,7 +27,7 @@ export default async function ResidentLoginPage() {
         </div>
 
         <p className="text-center text-xs text-muted-foreground">
-          Don&apos;t have an account? Visit your barangay hall to register.
+          Don&apos;t have an account? Visit your barangay hall to register. Test
         </p>
       </div>
     </main>
